45a5fb130b33271f7bf846e5fbca938cf9b95ae0
David Ngo Dev <13045581+DavidNgoDev@users.noreply.github.com>
2021-02-16 08:06:20 +1100

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo pacman -Syu
+sudo pacman -Syyy
+sudo pacman -S xf86-video-fbdev xorg xorg-xinit nitrogen picom alacritty firefox base-devel wpa_supplicant wireless_tools netctl
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git/
+makepkg -si
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+echo "Done. You may want to reboot for everything to take affect."
+reboot
